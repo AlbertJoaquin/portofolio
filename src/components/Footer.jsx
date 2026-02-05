07bfd75d9a6e921121a890376fbb9f15f2a3f284
Joaquin <agenselada2542ant@student.fatima.edu.ph>
2026-02-05 17:25:54 +0800

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import '../index.css';
+
+export default function Footer() {
+    return (
+        <footer className="mPort-Footer">
+            <p>&copy; 2026 AJ. All rights reserved.</p>
+        </footer>
+    );
+}
